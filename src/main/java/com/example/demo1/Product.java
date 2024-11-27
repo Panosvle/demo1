59b8c43ea9748d5387f424988e73ca25ad01f33e
Panagiotis-Georgios Vletsas <pgvletsas@athtech.gr>
2024-11-27 22:19:38 +0200

    1.0

---
+++ src/main/java/com/example/demo1/Product.java
@@ -0,0 +1,13 @@
+package com.example.demo1;
+
+public class Product {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
